6f0dad31fc21b32a3a8f547ba116ccddad7c3c87
Jake Whatley <jacob.whatley91@gmail.com>
2016-11-17 01:16:27 +0100

    jenkins-6.0: adjust for monthly security releases
        DONE:
    	- add back all devices from 6.0.1 nightlies
    	- whitespace cleanup
    	- fix branch in sync_devices.sh
    	- all nodes are using /home/build/.ccache,
    	  no need for additional check
    	- change buildtype to "SECURITY_RELEASE"
    	- remove unecessary script
    
    Change-Id: I633473b7aebce9f29b51cc170d25e251fa6dd3de

--- build_nightly_groovy.sh
+++ build_nightly_groovy.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
-if [ -d /ccache ]; then
-    export USE_CCACHE=1
-    export CCACHE_COMPRESS=1
-    export CCACHE_DIR=/ccache
-fi
 if [ -d /home/build/.ccache ]; then
     export USE_CCACHE=1
     export CCACHE_COMPRESS=1
     export CCACHE_DIR=/home/build/.ccache
 fi
 
-export ROM_BUILDTYPE=NIGHTLY
+export ROM_BUILDTYPE=SECURITY_RELEASE
 export BUILD_WITH_COLORS=0
 # for local repo
 export PATH=/home/build/bin:$PATH
-export ANDROID_JACK_VM_ARGS="-Xmx4g -Dfile.encoding=UTF-8 -XX:+TieredCompilation"
 
 DEVICE=$device
 
@@ -44,7 +38,6 @@ cd ../..
 repo forall -c "git reset --hard" -j48
 repo forall -c "git clean -fd" -j48
 repo sync --force-sync -cdf -j48
-# we should consider make installclean to decrease build times
 rm -rf out
 
 #use non-public keys to sign ROMs - keys not in git for obvious reasons
@@ -52,10 +45,6 @@ cp /home/build/.keys/* ./build/target/product/security
 
 . build/envsetup.sh
 brunch $DEVICE
-
-########## Temporary exit - remove when nightlies are ready ##########
-exit 0
-########## Temporary exit - remove when nightlies are ready ##########
 
 if [ $? -eq 0 ]; then
 	source upload_nightly.sh
