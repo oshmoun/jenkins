8c8dc93984f749cf293465bf4e5c369419dc9b6c
Humberto Borba <humberos@gmail.com>
2016-11-24 16:36:50 -0200

    jenkins: Always update repos before start a build
    Also, fix codestyle by replacing tabs to spaces
    and returns 0 if the script is ok.
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: Idcf6a7322c5e9cd3a7a5c464fe7dc6224fc0d5c2

--- build_nightly_groovy.sh
+++ build_nightly_groovy.sh
@@ -24,7 +24,7 @@ echo CCACHE_DIR=$CCACHE_DIR
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
 
 cd /home/build/omni-6
-
+repo sync -d -c -f --force-sync -j16
 rm -rf out
 
 #use non-public keys to sign ROMs - keys not in git for obvious reasons
@@ -34,9 +34,11 @@ cp /home/build/.keys/* ./build/target/product/security
 brunch $DEVICE
 
 if [ $? -eq 0 ]; then
-	cd /home/build/jenkins-android-6.0
-	source upload_build.sh
-#	/home/build/delta/omnidelta.sh $DEVICE
+    cd /home/build/jenkins-android-6.0
+    source upload_build.sh
+#    /home/build/delta/omnidelta.sh $DEVICE
 else
-	exit 1
+    exit 1
 fi
+
+exit 0
